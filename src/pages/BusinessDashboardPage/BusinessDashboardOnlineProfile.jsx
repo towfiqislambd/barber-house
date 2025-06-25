a6fa5950b09de46e3d2d123e08b2e6a7fb992593
towfiqislambd <towfiqislambd@gmail.com>
2025-06-25 12:59:49 +0600

    done

--- src/pages/BusinessDashboardPage/BusinessDashboardOnlineProfile.jsx
+++ src/pages/BusinessDashboardPage/BusinessDashboardOnlineProfile.jsx
@@ -1,61 +1,118 @@
 import useAuth from "@/hooks/useAuth";
 import { Link } from "react-router-dom";
-import { useGetStore, useStoreDetails } from "@/hooks/cms.queries";
-import { Loader } from "@/components/Loader/Loader";
+import { ContinueButtonArrowSvg } from "@/components/svgContainer/SvgContainer";
+import userImg from "../../../src/assets/images/online-profile/user.png";
+import chartImg from "../../../src/assets/images/online-profile/chart.png";
+import clientImg from "../../../src/assets/images/online-profile/client.png";
 
 const BusinessDashboardOnlineProfile = () => {
+  const steps = [
+    {
+      id: 1,
+      title: "Tell us about your business",
+      description:
+        "Shire some basic Info, ke your venue name, location and opening hours",
+      imgSrc: userImg,
+    },
+    {
+      id: 2,
+      title: "Stand out online",
+      description:
+        "Add images of your location and select some venue highlights to make your profile pop",
+      imgSrc: chartImg,
+    },
+    {
+      id: 3,
+      title: "Accept online bookings",
+      description:
+        "With a complete profile you're ready to start taking online bookinge directly and on the Fresha marketplace",
+      imgSrc: clientImg,
+    },
+  ];
   const { user } = useAuth();
-  const business_profile_id = user?.business_profile?.id;
-  const { data: get_online_store_id } = useGetStore(business_profile_id);
-  const { data: storeDetails, isLoading } = useStoreDetails(
-    get_online_store_id?.online_store_id
-  );
-
-  if (isLoading) {
-    return (
-      <div className="flex justify-center items-center h-[80vh]">
-        <Loader />
-      </div>
-    );
-  }
-
   return (
-    <Link
-      to={"/businessDashboard/tellusaboutyourbusiness"}
-      className="max-w-[900px] mx-auto xl:mt-10 block px-5 md:px-0 mt-6 md:mt-0"
-    >
-      <div className="flex items-center gap-[9px] justify-between">
-        <div className="max-w-[771px]">
-          <h1 className="text-textSecondary font-outfit text-[28px] font-semibold leading-[33.6px]">
-            Online profile
-          </h1>
-          <p className="mt-[9px] text-textColor font-manrope text-sm sm:text-base font-normal leading-6">
-            View and manage your online profile.
-          </p>
-        </div>
-      </div>
-      <div className="mt-8 flex flex-col lg:flex-row justify-between items-center lg:border border-[#DFE1E6] rounded-2xl">
-        <img
-          className="xl:w-[340px] lg:w-[300px] w-full xl:h-[207px] h-[200px] object-cover lg:rounded-l-2xl rounded-2xl"
-          src={`${import.meta.env.VITE_SITE_URL}/${
-            storeDetails?.store?.store_images[0]?.images
-          }`}
-          alt="Not Found"
-        />
-        <div className="xl:pr-8 lg:px-5 xl:px-0 w-full xl:w-[530px] mt-5 lg:mt-0">
-          <div className="flex items-start justify-between max-w-[589px]">
-            <div className="lg:w-[80%]">
-              <h1 className="text-textColor font-manrope text-lg font-bold leading-[27px]">
-                {storeDetails?.store?.name}
+    <>
+      {user?.business_profile?.online_store ? (
+        <Link
+          to={"/businessDashboard/tellusaboutyourbusiness"}
+          className="max-w-[900px] mx-auto xl:mt-10 block px-5 md:px-0 mt-6 md:mt-0"
+        >
+          <div className="flex items-center gap-[9px] justify-between">
+            <div className="max-w-[771px]">
+              <h1 className="text-textSecondary font-outfit text-[28px] font-semibold leading-[33.6px]">
+                Online profile
               </h1>
-              <p className="text-[#757575] font-manrope text-base font-medium leading-6">
-                {storeDetails?.store?.address}
+              <p className="mt-[9px] text-textColor font-manrope text-sm sm:text-base font-normal leading-6">
+                View and manage your online profile.
               </p>
             </div>
           </div>
-        </div>
-      </div>
-    </Link>
+          <div className="mt-8 flex flex-col lg:flex-row justify-between items-center lg:border border-[#DFE1E6] rounded-2xl">
+            <img
+              className="xl:w-[340px] lg:w-[300px] w-full xl:h-[207px] h-[200px] object-cover lg:rounded-l-2xl rounded-2xl"
+              src={`${import.meta.env.VITE_SITE_URL}/${
+                user?.business_profile?.online_store?.store_images[0]?.images
+              }`}
+              alt="Not Found"
+            />
+            <div className="xl:pr-8 lg:px-5 xl:px-0 w-full xl:w-[530px] mt-5 lg:mt-0">
+              <div className="flex items-start justify-between max-w-[589px]">
+                <div className="lg:w-[80%]">
+                  <h1 className="text-textColor font-manrope text-lg font-bold leading-[27px]">
+                    {user?.business_profile?.online_store?.name}
+                  </h1>
+                  <p className="text-[#757575] font-manrope text-base font-medium leading-6">
+                    {user?.business_profile?.online_store?.address}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Link>
+      ) : (
+        <section className="max-w-[1320px] mx-auto px-5 lg:px-0 mt-5 md:mt-0">
+          <div className="flex justify-end gap-4">
+            <Link
+              to={"/businessDashboard/businessContainer"}
+              className="bg-[#0D1619] text-[#FFF] text-base px-[18px] py-[10px] rounded-[10px] flex items-center justify-center gap-[6px]"
+            >
+              Continue
+              <ContinueButtonArrowSvg />
+            </Link>
+          </div>
+          <div className="3xl:mt-[90px] 2xl:mt-8 lg:mt-11 mt-5 flex flex-col lg:flex-row items-center 2xl:gap-12 xl:gap-5 lg:gap-10 justify-center">
+            {/* This is left side div */}
+            <div className="xl:max-w-[486px] lg:max-w-[300px] max-w-[500px]">
+              <h1 className="text-[#000] font-outfit text-2xl md:text-3xl lg:text-5xl xl:text-5xl font-semibold xl:leading-[57.6px] lg:leading-[50px]">
+                Let’s get you published on the world’s{" "}
+                <span className="bg-primary-gradient bg-clip-text text-transparent">
+                  most popular marketplace
+                </span>{" "}
+                to grow your business
+              </h1>
+            </div>
+            {/* This is right side div */}
+            <div className="md:mt-8 mt-4 lg:mt-0">
+              {steps.map(step => (
+                <div key={step.id} className="flex gap-6 2xl:py-8 py-4 lg:px-2">
+                  <div>
+                    <h1 className="text-textColor font-outfit text-xl md:text-2xl font-semibold leading-[28.8px]">
+                      {step.id} {step.title}
+                    </h1>
+                    <p className="text-[#757575] font-manrope text-base font-medium leading-6 md:mt-4 max-w-[341px] md:ml-4">
+                      {step.description}
+                    </p>
+                  </div>
+                  <div>
+                    <img src={step.imgSrc} alt="Step Image" />
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </section>
+      )}
+    </>
   );
 };
 
